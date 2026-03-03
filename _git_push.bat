81f2c54c34f7da58f86a39de57db0e614a99d28b
vbord <s_bord@hotmail.com>
2026-03-03 10:04:04 -0500

    Initial commit: AudioPWA + server_code

--- _git_push.bat
+++ _git_push.bat
@@ -1,8 +1,11 @@
 @echo off
 cd /d E:\vs2022\mvc\AudioPWA
+git init
+git add .
+git commit -m "Initial commit: AudioPWA + server_code"
 
 echo Setting GitHub remote...
-git remote remove origin 2>nul
+git remote remove origin 2 
 git remote add origin https://github.com/vbord/AudioPWA.git
 
 echo Renaming branch to main...
